afdaf7286753a871613c4677cecff50faa95d31a
artlessnick <artlessnick@gmail.com>
2021-08-30 19:46:19 +0300

    Add test for boards and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  namespace :web do
-    get 'sessions/new'
-  end
-  namespace :web do
-    get 'boards/show'
+  root :to => "web/boards#show"
+
+  scope module: :web do
+    resource :board, only: :show
+    resource :session, only: :new
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
