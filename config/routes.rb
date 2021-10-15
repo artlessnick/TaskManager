6cab169ab5164720905a9c0b8a5657117f9706a5
artlessnick <artlessnick@gmail.com>
2021-10-15 14:45:46 +0300

    User Api

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,10 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  namespace :api, defaults: {format: :json}  do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      resources :users, only: [:index, :show]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
