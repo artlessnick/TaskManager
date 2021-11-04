6c7fdf0276368911dd95752017ec54fa3a0d11e6
artlessnick <artlessnick@gmail.com>
2021-11-04 18:32:39 +0300

    Add gem letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.rails_logger = true
   end
+
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_caching = true
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
